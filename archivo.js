ab1ee93f339061ffae3529ab89f0ab49e689c0b0
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-07-17 12:30:28 -0700

    Peso planetas

--- archivo.js
+++ archivo.js
@@ -1,1 +1,19 @@
 //CREAR FUNCIONES
+var peso = parseFloat(prompt("Ingresa tu peso"));
+pesoPlaneta();
+
+function pesoPlaneta(){
+    var marte=3.71, mercurio=3.7, jupiter=24.79, venus=8.87;
+    var tierraG = 9.78;
+
+    var martePeso = (peso/tierraG) * marte;
+    var mercurioPeso = (peso/tierraG) * mercurio;
+    var jupiterPeso = (peso/tierraG) * jupiter;
+    var venusPeso = (peso/tierraG) * venus;
+
+    alert("Tu peso en Marte es: "+ martePeso.toFixed(2) + "\n"+ 
+          "Tu peso en Mercurio es: " + jupiterPeso.toFixed(2) + "\n" +
+          "Tu peso en Jupiter es: " + mercurioPeso.toFixed(2) + "\n" +
+          "Tu peso en Venus es: "+ venusPeso.toFixed(2) );
+
+}
